6b5fa6024ef3088741acbc41e6370dfa091bac0d
JOSUE MACHADO <jeikerjosue@gmail.com>
2024-03-15 13:06:36 -0300

    pruebas con las props

---
+++ src/components/child.jsx
@@ -0,0 +1,9 @@
+export const child = (props) => {
+  return (
+    <>
+      <h1>{props.nombre}</h1>
+      <h2>{props.edad}</h2>
+      <h3>{props.sexo}</h3>
+    </>
+  );
+};
---
+++ src/components/father.jsx
@@ -0,0 +1,4 @@
+import { child } from "./child";
+export const father = () => {
+  rerturn(<child nombre="macarena" edad={25} sexo="femenino" />);
+};
